ba5180b8c69d17c057a6dc85de939b711c9e647a
Revanth <princechalapathi16@gmail.com>
2019-03-28 04:07:38 +0000

    Import a Build Script

---
+++ build.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/ChalapathiRevanth/Toolchain ~/Toolchain
+export ARCH=arm64
+export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
+export KBUILD_BUILD_USER=RevanthTolety
+export KBUILD_BUILD_HOST=thunderstorm-buildserver
+mkdir output
+make -C $(pwd) O=output fih6737m_65_n_defconfig
+make -j32 -C $(pwd) O=output
